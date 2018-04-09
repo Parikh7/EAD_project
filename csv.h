b0a98addbc564f6b4a491bbdb48ac8baba98e437
Parikh oberoi <parikhoberoi330@gmail.com>
2018-04-09 14:25:31 +0530

    Add files via upload

---
+++ csv.h
@@ -0,0 +1,11 @@
+//
+//  csv.h
+//  qwerty
+//
+//  Created by Parikh Oberoi on 21/01/18.
+//  Copyright © 2018 Parikh Oberoi. All rights reserved.
+//
+#pragma once
+
+void read_csv( const char* file_name, int nentries, float labels[nentries], float pixels[nentries][784] );
+
